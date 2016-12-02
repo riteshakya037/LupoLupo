da643a20a5a1e7429405990a9e6a2a2b1b81219e
Ritesh Shakya <riteshakya037@gmail.com>
2016-12-02 13:58:48 +0545

    initial Comit

---
+++ app/src/main/java/com/blues/lupolupo/views/activities/MainActivity.java
@@ -0,0 +1,10 @@
+package com.blues.lupolupo.views.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Ritesh Shakya
+ */
+
+class MainActivity extends AppCompatActivity {
+}
